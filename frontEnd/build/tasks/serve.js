aba25d9d5873ba1cf48c895f2a8ad20d20f5b906
bcrusu <bcrusu@users.noreply.github.com>
2016-05-09 16:48:55 +0300

    working on Linkedin OAuth integration...

--- frontEnd/build/tasks/serve.js
+++ frontEnd/build/tasks/serve.js
@@ -3,12 +3,12 @@ var browserSync = require('browser-sync');
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
-// at http://localhost:9000
+// at http://localhost:3000
 gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9000,
+    port: 3000,
     server: {
       baseDir: ['.'],
       middleware: function(req, res, next) {
@@ -21,12 +21,12 @@ gulp.task('serve', ['build'], function(done) {
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
-// at http://localhost:9000
+// at http://localhost:3000
 gulp.task('serve-bundle', ['bundle'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9000,
+    port: 3000,
     server: {
       baseDir: ['.'],
       middleware: function(req, res, next) {
--- frontEnd/src/authConfig.ts
+++ frontEnd/src/authConfig.ts
@@ -2,8 +2,17 @@ var baseConfig = {
     baseUrl: 'http://localhost:7777',  // backend server base address
     endpoint: null,  // use Aurelia HttpClient instead of 'aurelia-api' client
     configureEndpoints: [],
-    linkedin: {
-        clientId: '77d08hsurfp6gr'
+    loginRedirect: '#/customer',
+    logoutRedirect: '#/',
+    loginRoute: '/login',
+    loginOnSignup: true,
+    providers: {
+        linkedin: {
+            url: 'api/Account/LoginLinkedin',
+            clientId: '77d08hsurfp6gr',
+            scope: ['r_emailaddress', 'r_basicprofile'],
+            state: 'll'
+        }
     }
 };
 
