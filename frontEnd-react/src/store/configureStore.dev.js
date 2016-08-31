48382f39c7f2764e566b329ae3dfa34c2c55bced
bcrusu <bcrusu@users.noreply.github.com>
2016-08-31 17:59:04 +0300

    switched to redux-devtools-extension (cleaner code)

--- frontEnd-react/src/store/configureStore.dev.js
+++ frontEnd-react/src/store/configureStore.dev.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
-import DevTools from '../containers/DevTools'
 
 export default function configureStore(preloadedState) {
   const store = createStore(
@@ -10,7 +9,7 @@ export default function configureStore(preloadedState) {
     preloadedState,
     compose(
       applyMiddleware(thunk, createLogger()),
-      DevTools.instrument()
+      window.devToolsExtension ? window.devToolsExtension() : f => f
     )
   )
 
