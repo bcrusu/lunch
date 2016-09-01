5b99be06d48e4d19880d206fb856a21cbfd6164c
bcrusu <bcrusu@users.noreply.github.com>
2016-09-01 23:48:00 +0300

    merged dev & prod store configurations added redux-saga

--- frontEnd-react/src/configureStore.js
+++ frontEnd-react/src/configureStore.js
@@ -1,5 +1,49 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from './rootReducer'
+import createSagaMiddleware from 'redux-saga'
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+export default function configureStore(preloadedState) {
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    getEnhancer()
+  )
+
+  if (isDev)
+    enableHotModuleReloading(store)
+
+  return store
+}
+
+function getEnhancer() {
+  const sagaMiddleware = createSagaMiddleware()
+  let middleware = [thunk, sagaMiddleware]
+  
+  if (isDev)
+    middleware = [...middleware, createLogger()]
+
+  let enhancer = applyMiddleware(...middleware)
+
+  if (isDev) {
+    let devToolsExtension = window.devToolsExtension ? window.devToolsExtension() : f => f
+    enhancer = compose(enhancer, devToolsExtension)
+  }
+
+  return enhancer
+}
+
+function enableHotModuleReloading(store) {
+  if (module.hot) {
+    // TODO: review hmr - should explicitly include the public/profile reducers also? 
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('./rootReducer', () => {
+      const nextRootReducer = require('./rootReducer').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
 }
